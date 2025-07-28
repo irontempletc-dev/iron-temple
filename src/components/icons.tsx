970315a1dee77d8e1b6123b552adf09650307ce2
Iron Temple Training Center <irontempletc@gmail.com>
2025-07-28 18:43:05 +0000

    Thi is the color scheme and design used for our learning website.  Incor

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -9,11 +9,11 @@ export function IronTempleLogo(props: SVGProps<SVGSVGElement>) {
       aria-label="Iron Temple Logo"
     >
       <path
-        fill="hsl(var(--primary))"
+        fill="hsl(var(--accent))"
         d="M128 24a104 104 0 1 0 104 104A104.11 104.11 0 0 0 128 24Zm0 192a88 88 0 1 1 88-88a88.1 88.1 0 0 1-88 88Z"
       />
       <path
-        fill="hsl(var(--accent))"
+        fill="hsl(var(--primary))"
         d="M168 88H88a8 8 0 0 0-8 8v64a8 8 0 0 0 16 0v-28h64v28a8 8 0 0 0 16 0V96a8 8 0 0 0-8-8Zm-8 48h-64v-32h64Z"
       />
     </svg>
