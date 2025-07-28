d28f38b4d427991c939d625146b13a73682150a3
Iron Temple Training Center <irontempletc@gmail.com>
2025-07-28 18:54:11 +0000

    there is no public folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,8 +20,10 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased min-h-screen bg-background">
-        {children}
+      <body>
+        <div className="font-body antialiased min-h-screen bg-background/80 dark:bg-background/90">
+          {children}
+        </div>
         <Toaster />
       </body>
     </html>
