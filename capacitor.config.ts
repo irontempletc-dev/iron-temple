25c20801b38cdb9dd94b7899e8d63fca5038c4f1
Iron Temple Training Center <irontempletc@gmail.com>
2025-08-12 15:22:51 +0000

    but I want to keep the web application and start a new mobile applicatio

---
+++ capacitor.config.ts
@@ -0,0 +1,11 @@
+
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.irontemple.traininghub',
+  appName: 'Iron Temple Training Hub',
+  webDir: 'out',
+  bundledWebRuntime: false,
+};
+
+export default config;
